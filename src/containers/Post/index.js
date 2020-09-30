9fa7988942a07b9832e3b2930464cf6ab1c5b432
skooma1992 <upsonjake@gmail.com>
2020-09-30 14:22:19 -0400

    styling

--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./style.css";
+import Card from "../../components/UI/Card";
 
 const Post = props => {
-  return <div></div>;
+  return (
+    <section className='container'>
+      <div className='blogPost'></div>
+      <div className='sidebar'></div>
+    </section>
+  );
 };
 
 export default Post;
